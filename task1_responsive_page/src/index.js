1f75bd85ce9ee91b31b1f4a8dda0b1cde6b3a243
Choco <trust2065@gmail.com>
2019-10-18 16:08:47 +1100

    task 1

---
+++ task1_responsive_page/src/index.js
@@ -0,0 +1,25 @@
+new Vue({
+  el: "#app",
+  data: {
+    dataList: [
+      {
+        imageUrl: "http://via.placeholder.com/400x300",
+        alt: "placeHolder",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eliusmod tempor incidint ul labore et dolore magna aliquia, Ut enum ad minim veniam, quis nostrud experitaition ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      },
+      {
+        imageUrl: "http://via.placeholder.com/400x300",
+        alt: "placeHolder",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eliusmod tempor incidint ul labore et dolore magna aliquia."
+      },
+      {
+        imageUrl: "http://via.placeholder.com/400x300",
+        alt: "placeHolder",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eliusmod tempor incidint ul labore et dolore magna aliquia, Ut enum ad minim veniam, quis nostrud experitaition ullamco."
+      }
+    ]
+  }
+});
